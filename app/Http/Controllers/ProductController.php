8f9d4839d164e5194be19aa863024d178bb0044a
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-02-27 12:47:27 +0600

    add product in cart tbl(DB)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,17 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
 
 class ProductController extends Controller
 {
+    //Products slide
     function index()
     {
         $data=Product::all();
         return view('product',['products'=>$data]);
     }
+    //Trending product
     function detail($id)
     {
         $data= Product::find($id);
         return view('detail',['product'=>$data]);
+    }
+    //Add products in cart table
+    function addToCart(Request $req)
+    {
+        if($req->session()->has('user'))
+        {
+            $cart= new Cart;
+            $cart->user_id=$req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
+
+        }
+        else
+        {
+            return redirect('/login');
+        }
     }
 }

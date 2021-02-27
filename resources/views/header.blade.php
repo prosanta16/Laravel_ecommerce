4544b43a82349b5edb562acf0b23e2381792f16a
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-02-27 18:42:10 +0600

    login and logout cmplte

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -33,17 +33,17 @@ if(Session::has('user'))
         </form>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="/cartlist">cart({{$total}})</a></li>
-          
+          @if(Session::has('user'))
           <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">OK
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{session::get('user')['name']}}
             <span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="/logout">Logout</a></li>
             </ul>
           </li>
-          
+          @else
           <li><a href="/login">Login</a></li>
-          
+          @endif
         </ul>
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
